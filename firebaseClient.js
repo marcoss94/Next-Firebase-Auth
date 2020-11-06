a7e848c7f31574a351a0de962445d738778c17b0
marcos <marcos.alvarez@gmail.copm>
2020-11-06 16:27:29 -0300

    agregando firebase

---
+++ firebaseClient.js
@@ -0,0 +1,17 @@
+const firebase = require("firebase");
+
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyD7Y7SHL0j-nSu1L6GS1xTTTVBs9FXD078",
+  authDomain: "next-firebase-auth-8e6d4.firebaseapp.com",
+  databaseURL: "https://next-firebase-auth-8e6d4.firebaseio.com",
+  projectId: "next-firebase-auth-8e6d4",
+  storageBucket: "next-firebase-auth-8e6d4.appspot.com",
+  messagingSenderId: "2581425950",
+  appId: "1:2581425950:web:7cf9aa8911f62ba4c2a30f",
+};
+
+export default function firebaseClient() {
+  if (firebase.apps.lenght) {
+    firebase.initializeApp(FIREBASE_CONFIG);
+  }
+}
